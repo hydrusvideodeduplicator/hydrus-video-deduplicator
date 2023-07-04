14d1d2e4cefad22e0ed47ab2f0642f04c70cc2c8
Yochyo <herodragon25@gmail.com>
2023-07-04 21:46:51 +0200

    Add Docker container (#19)

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM ubuntu:latest
+WORKDIR /usr/src/app
+RUN apt-get update && apt-get install -y \
+    python3.11 \
+    python3.11-dev \
+    python3-pip \
+    git \
+    make \
+    sqlite3 \
+    ffmpeg \
+    libavcodec-dev \
+    libavfilter-dev \
+    && rm -rf /var/lib/apt/lists/*
+RUN python3.11 -m pip install hydrusvideodeduplicator
+COPY ./docker-entrypoint.sh ./entrypoint.sh
+
+ENV DEDUP_DATABASE_DIR=/usr/src/app/db
+ENV API_URL=https://host.docker.internal:45869
+CMD ["./entrypoint.sh"]
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+Command="python3.11 -m hydrusvideodeduplicator"
+[[ -n "${API_KEY}" ]] && Command="${Command} --api-key='${API_KEY}'"
+[[ -n "${API_URL}" ]] && Command="${Command} --api-url='${API_URL}'"
+[[ -n "${THRESHOLD}" ]] && Command="${Command} --threshold=${THRESHOLD}"
+
+[[ ${CERT} = "true" ]] && Command="${Command} --verify-cert=cert"
+
+[[ ${OVERWRITE} = "true" ]] && Command="${Command} --overwrite" || Command="${Command} --no-overwrite"
+[[ ${SKIP_HASHING} = "true" ]] && Command="${Command} --skip-hashing" || Command="${Command} --no-skip-hashing"
+[[ ${CLEAR_SEARCH_CACHE} = "true" ]] && Command="${Command} --clear-search-cache" || Command="${Command} --no-clear-search-cache"
+[[ ${VERBOSE} = "true" ]] && Command="${Command} --verbose" || Command="${Command} --no-verbose"
+echo "${Command}"
+eval "${Command}"

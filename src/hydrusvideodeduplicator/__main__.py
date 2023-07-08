b863fa8ad811af22ea717e4c760fd85bebe2666e
appleappleapplenanner <applenannerapple@gmail.com>
2023-07-08 11:26:31 -0700

    Remove old constant, set default local file service to None
    tag_service_name was from I was using VideoHash.
    
    local_file_service defaults to "all_local_files" in the constructor
    so it's not needed here.
    I would use the actual service key for all_local_files but it's ugly
    in --help because
    service keys are
    long hex strings.

--- src/hydrusvideodeduplicator/__main__.py
+++ src/hydrusvideodeduplicator/__main__.py
@@ -28,8 +28,9 @@ def main(
     api_url: Annotated[Optional[str], typer.Option(help="Hydrus API URL")] = HYDRUS_API_URL,
     overwrite: Annotated[Optional[bool], typer.Option(help="Overwrite existing perceptual hashes")] = False,
     query: Annotated[Optional[List[str]], typer.Option(help="Custom Hydrus tag query")] = HYDRUS_QUERY,
-    file_service_key: Annotated[Optional[str], typer.Option(
-        help="Local file service key for searching specific file domains")] = HYDRUS_LOCAL_FILE_SERVICE_KEY,
+    file_service_key: Annotated[
+        Optional[str], typer.Option(help="Local file service key")
+    ] = HYDRUS_LOCAL_FILE_SERVICE_KEY,
     threshold: Annotated[
         Optional[float], typer.Option(help="Similarity threshold for a pair of videos where 100 is identical")
     ] = 75.0,
@@ -45,7 +46,6 @@ def main(
     verbose: Annotated[Optional[bool], typer.Option(help="Verbose logging")] = False,
     debug: Annotated[Optional[bool], typer.Option(hidden=True)] = False,
 ):
-
     # CLI debug parameter sets log level to info or debug
     loglevel = logging.WARNING
     if debug:

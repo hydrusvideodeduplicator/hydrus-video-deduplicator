a2036c8825f9422ee800b266acc1f0d4d7c4feef
ianwal <52143079+ianwal@users.noreply.github.com>
2023-07-10 01:21:47 -0700

    Add test for similarity, change test videos structure
    Similar videos should be prefixed S and a group number e.g. S01
    
    Strange videos should go in strange. They're just for hashing. They could
    or could not be similar to other videos
    so they need to be separated from the rest.

---
+++ tests/test_vpdqpy.py
@@ -0,0 +1,87 @@
+"""
+
+These tests use clips from the Big Buck Bunny movie,
+which is licensed under Creative Commons Attribution 3.0
+(https://creativecommons.org/licenses/by/3.0/).
+(c) copyright 2008, Blender Foundation / www.bigbuckbunny.org
+Blender Foundation | www.blender.org
+
+"""
+
+import unittest
+from pathlib import Path
+
+from hydrusvideodeduplicator.vpdqpy.vpdqpy import Vpdq
+
+
+class TestVpdq(unittest.TestCase):
+    def setUp(self):
+        all_vids_dir = Path(__file__).parent / "videos"
+        vids_dirs = ["big_buck_bunny"]
+        self.all_vids: list[Path] = []
+        for vids_dir in vids_dirs:
+            self.all_vids.extend(Path(all_vids_dir / vids_dir).glob("*"))
+
+        # Strange vids are videos that should hash but not be compared.
+        # They're used to test that the program doesn't crash
+        # when it encounters a video with odd characteristics like extremely short length
+        # or a video that has tiny dimensions, etc. The more of these added the better.
+        # They shouldn't be compared because they might be similar to other videos, but not all of them in a group.
+        strange_vids_dir = "strange"
+        self.strange_vids: list[Path] = Path(all_vids_dir / strange_vids_dir).glob("*")
+
+    # Return if two videos are supposed to be similar
+    # This uses the prefix SXX where XX is an abitrary group number
+    # If two videos have the same SXX they should be similar
+    # If they don't they should NOT be similar
+    def similar_group(self, vid1: Path, vid2: Path) -> bool:
+        # If either video doesn't have a group, they're not similar
+        if vid1.name.split("_")[0][0] != "S" or vid2.name.split("_")[0][0] != "S":
+            return False
+
+        vid1_group = vid1.name.split("_")[0]
+        vid2_group = vid2.name.split("_")[0]
+        return vid1_group == vid2_group
+
+    # Hash videos
+    def calc_hashes(self, vids: list[Path]) -> dict[Path, list[Vpdq]]:
+        vids_hashes = {}
+        for vid in vids:
+            perceptual_hash = Vpdq.computeHash(vid)
+            self.assertTrue(len(perceptual_hash) > 0)
+
+            perceptual_hash_json = Vpdq.vpdq_to_json(perceptual_hash)
+            self.assertTrue(perceptual_hash_json != "[]")
+            vids_hashes[vid] = perceptual_hash
+        return vids_hashes
+
+    # Hash all videos. They should all have hashes.
+    def test_hashing(self):
+        print("Hashing:")
+        self.calc_hashes(self.all_vids)
+        self.calc_hashes(self.strange_vids)
+
+    # Compare similar videos. They should be similar.
+    def test_compare_similarity_true(self):
+        print("Comparing similarity:")
+        vids_hashes = self.calc_hashes(self.all_vids)
+        for vid1 in vids_hashes.items():
+            print(vid1[0].name)
+            for vid2 in vids_hashes.items():
+                if vid1[0] == vid2[0]:
+                    continue
+
+                vpdq_hash1 = vid1[1]
+                vpdq_hash2 = vid2[1]
+
+                similar, similarity = Vpdq.is_similar(vpdq_hash1, vpdq_hash2)
+                self.assertTrue(0 <= similarity <= 100)
+
+                if self.similar_group(vid1[0], vid2[0]):
+                    self.assertTrue(similar, f"{vid1[0].name}, {vid2[0].name}")
+                else:
+                    self.assertFalse(similar, f"{vid1[0].name}, {vid2[0].name}")
+
+
+if __name__ == "__main__":
+    unittest.main()
